46d9bf2ef6f555b5267e83b6083192065bde7076
YinchengChen23 <65726063+YinchengChen23@users.noreply.github.com>
2023-10-29 05:52:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='ADlasso',
+    name='PreLect',
     version='0.0.1', #Jan-25-2023
-    packages=['ADlasso',],
+    packages=['PreLect',],
     license='MIT',
     author='yincheng_chen',
     author_email = 'yin.cheng.23@gmail.com',
-    url = 'https://github.com/YinchengChen23/ADlasso',
+    url = 'https://github.com/YinchengChen23/PreLect',
     setup_requires = ['numpy>=1.17','torch'],
     install_requires=['numpy>=1.17','torch','pandas>=0.25','scipy','scikit-learn>=1.2', 'matplotlib','seaborn'],
     long_description=long_description,
